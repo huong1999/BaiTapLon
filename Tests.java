521c5c21e6570c6644fa2c9e244d3df037fede5b
huong1999 <37472308+huong1999@users.noreply.github.com>
2018-10-02 20:36:47 +0700

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -2,13 +2,9 @@ package com.company;
 
 public class Tests {
     public static void main(String[] args) {
-        Word keyword1 = new Word();
-        DictionaryCommandLine listWord = new DictionaryCommandLine();
-//        DictionaryManagement nhap1 = new DictionaryManagement();
-//        nhap1.insertFromCommandline(keyword1);
-        listWord.addWord(keyword1);
-//        listWord.addWord(keyword);
 
+        DictionaryCommandLine listWord = new DictionaryCommandLine();
         listWord.dictionaryBasic();
+
     }
 }
