f43ce3640602ba69a11a53e26ceb5a920d4f56f0
huong1999 <37472308+huong1999@users.noreply.github.com>
2018-10-02 20:27:18 +0700

    Update DictionaryCommandLine.java

--- DictionaryCommandLine.java
+++ DictionaryCommandLine.java
@@ -1,32 +1,32 @@
 package com.company;
 
 public class DictionaryCommandLine extends Dictionary {
-    public void showAllWord(){
+    public void showAllWord() {
         int i;
+        System.out.println(" No     |English    |Vietnamese");
+        for (i = 1; i <= 3; i++) {
 
-        for (i=0; i< this.list.size(); i++)
-        {
-
-            System.out.print(" "+i);
+            System.out.print(" " + (i));
             System.out.print("         |");
-            System.out.print(list.get(i).getWord_target());
+            System.out.print(list.get(i-1).getWord_target());
             System.out.print("      |");
-            System.out.println(list.get(i).getWord_explain());
+            System.out.println(list.get(i-1).getWord_explain());
 
         }
     }
-    public void dictionaryBasic(){
-        Word keyword = new Word();
-        DictionaryManagement nhap = new DictionaryManagement();
-        int i;
-        for(i=0; i<this.list.size(); i++) {
-            nhap.insertFromCommandline(keyword);
 
-        }
-        System.out.println(" No        |English      |Vietnamese");
+    public void dictionaryBasic() {
 
+        Word keyword1 = new Word();
+        Word keyword2 = new Word();
+        Word keyword3 = new Word();
+        DictionaryManagement input = new DictionaryManagement();
+        input.insertFromCommandline(keyword1);
+        addWord(keyword1);
+        input.insertFromCommandline(keyword2);
+        addWord(keyword2);
+        input.insertFromCommandline(keyword3);
+        addWord(keyword3);
         this.showAllWord();
     }
-
-
 }
