ebed097f32a93d0e51461a1e433a0e2222b7680f
lananhnguyen17 <43380474+lananhnguyen17@users.noreply.github.com>
2018-10-03 22:07:27 +0700

    Update DictionaryManagement.java

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -3,18 +3,26 @@ package com.company;
 import java.util.Scanner;
 
 public class DictionaryManagement {
-    public void insertFromCommandline(Word nhapTu) {
-        Dictionary add = new Dictionary();
-
+    
+    public void insertFromCommandline(Dictionary ad) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("So luong tu can nhap : ");
+        int size = scan.nextInt();
+        String target,explain;
+        scan.nextLine();
+        for(int i = 0; i < size; i++) {
+            Word nhapTu= new Word();
+            System.out.print("Nhap tu moi : ");
 
-        System.out.print("Nhap tu moi : ");
-        String target = scan.nextLine();            //nhap tu tieng Anh
-        nhapTu.setWord_target(target);
-        System.out.print("Nhap nghia : ");
-        String explain = scan.nextLine();           //nhap nghĩa tieng Viet
-        nhapTu.setWord_explain(explain);
-        //scan.close();
+            target = scan.nextLine();            //nhap tu tieng Anh
+            nhapTu.setWord_target(target);
+
+            System.out.print("Nhap nghia : ");
+
+            explain = scan.nextLine();           //nhap nghĩa tieng Viet
+            nhapTu.setWord_explain(explain);
+            ad.list.add(nhapTu);
+        }
 
     }
 }
