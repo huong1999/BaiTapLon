74201d7ee1fc4e1e6677b19915acadefed16c79a
lananhnguyen17 <43380474+lananhnguyen17@users.noreply.github.com>
2018-10-03 22:01:18 +0700

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -2,11 +2,6 @@ package com.company;
 
 import java.util.ArrayList;
 
-public class Dictionary {
-
-    ArrayList<Word> list = new ArrayList<Word>();
-
-    public void addWord(Word word) {
-        list.add(word);
-    }
+public class Dictionary {  
+    ArrayList <Word> list = new ArrayList <Word>();
 }
