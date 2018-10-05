871cd44f78a36c2c89c4269fb1bbc9f2746256b6
dat-lq-234 <43435394+dat-lq-234@users.noreply.github.com>
2018-10-05 18:37:03 +0700

    Update DictionaryManagement.java

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -1,17 +1,18 @@
 package com.company;
 
 import java.util.Scanner;
+import java.io.File;
 
 public class DictionaryManagement {
-    
+
     public void insertFromCommandline(Dictionary ad) {
         Scanner scan = new Scanner(System.in);
         System.out.print("So luong tu can nhap : ");
         int size = scan.nextInt();
-        String target,explain;
+        String target, explain;
         scan.nextLine();
-        for(int i = 0; i < size; i++) {
-            Word nhapTu= new Word();
+        for (int i = 0; i < size; i++) {
+            Word nhapTu = new Word();
             System.out.print("Nhap tu moi : ");
 
             target = scan.nextLine();            //nhap tu tieng Anh
@@ -22,6 +23,51 @@ public class DictionaryManagement {
             explain = scan.nextLine();           //nhap nghĩa tieng Viet
             nhapTu.setWord_explain(explain);
             ad.list.add(nhapTu);
+        }
+
+    }
+
+    public void insertFromFile(Dictionary ad) {
+
+        try (Scanner sc = new Scanner(new File("dictionaries.txt"))) {
+            String str;
+
+            while (sc.hasNext()) {
+
+                str = sc.nextLine();
+                Word nhapTu = new Word();
+                int idx = 0;
+                for (int i = 0; i < str.length(); i++) {
+
+                    if (str.charAt(i) == '\t') {
+                        idx = i;
+                        break;
+                    }
+                }
+                
+                String vn = str.substring(0, idx);
+                String en = str.substring(idx + 1);
+                nhapTu.setWord_target(vn);
+                nhapTu.setWord_explain(en);
+                ad.list.add(nhapTu);
+            }
+            
+        } catch (Exception e) {
+        }
+    }
+
+    public void dictionaryLookup(Dictionary ad) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Nhap tu can tim ");
+        String find = scan.nextLine();
+        int num = 1;
+
+        for (Word element : ad.list) {
+            if (element.getWord_target().contains(find)) {
+                System.out.printf("|%-7d|%-50s|%-50s|\n",num, element.getWord_target(), element.getWord_explain());
+            } else {
+                num++;
+            }
         }
 
     }
