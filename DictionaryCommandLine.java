fb560bf0f4907ccab57d543d8ef6fb842f094a6c
lananhnguyen17 <43380474+lananhnguyen17@users.noreply.github.com>
2018-10-07 23:51:08 +0700

    Update Version 3

--- DictionaryCommandLine.java
+++ DictionaryCommandLine.java
@@ -1,36 +1,59 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class DictionaryCommandLine{
-    Dictionary word = new Dictionary();
-    public void showAllWord(Dictionary ad) {
-        
+    
+    //Phien ban so khai
+    public void showAllWord(Dictionary ad) {   
         System.out.println(" No         |English    |Vietnamese");
-        int STT = 1;
+        int j = 1;
         for (int i = 0; i < ad.list.size(); i++) {
 
-            System.out.print(" " + STT);
+            System.out.print(" " + j);
             System.out.print("          |");
             System.out.print(ad.list.get(i).getWord_target());
             System.out.print("         |");
             System.out.println(ad.list.get(i).getWord_explain());
-            STT++;
+            j++;
         }
     }
-
+    
     public void dictionaryBasic() {
         Dictionary word = new Dictionary();
         DictionaryManagement input = new DictionaryManagement();     
-        input.insertFromCommandline(word);// them tu
-        showAllWord(word);   //in ra cac tu da nhap
+        input.insertFromCommandline(word);
+        showAllWord(word);
     }
     
+    //Phien ban cai tien lan 1
     public void dictionaryAdvanced() {
         Dictionary word = new Dictionary();
-        DictionaryManagement input = new DictionaryManagement();     
+        DictionaryManagement input = new DictionaryManagement();
         input.insertFromFile(word);
-        showAllWord(word);   //in ra cac tu da nhap
-        
         input.dictionaryLookup(word);
-        
+        showAllWord(word);
+    }
+    
+    //Phien ban cai tien lan 2
+    public void dictionarySearcher(Dictionary ad) {
+        System.out.print("Nhap tu can tim : ");
+        Scanner scan = new Scanner(System.in);
+        String target = scan.nextLine();
+        int mark = 1;
+        for (Word words : ad.list)
+        {
+            if (words.getWord_target().contains(target))
+            {
+               System.out.println(mark +")     " + words.getWord_target() + '\t' + words.getWord_explain());
+               mark ++;
+            }
+            else {
+                continue;
+            }
+        }
+        if (mark == 1) {
+            System.out.println("Khong tim thay tu can nhap");
+        }
     }
 }
