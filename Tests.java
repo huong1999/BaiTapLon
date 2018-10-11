5df14e975f8f3c93dfcc505dcce2b99607f8b720
lananhnguyen17 <43380474+lananhnguyen17@users.noreply.github.com>
2018-10-11 16:23:22 +0700

    Sua Tieng Viet khong dau

--- Tests.java
+++ Tests.java
@@ -8,17 +8,17 @@ public class Tests {
        Dictionary ad = new Dictionary();
        DictionaryManagement input = new DictionaryManagement();
        input.insertFromFile(ad);
-       System.out.println("TỪ ĐIỂN ANH - VIỆT");
-       System.out.println("Các chức năng : ");
-       System.out.println("1) Tra Từ ");
-       System.out.println("2) Hiển thị toàn bộ dữ liệu trong từ điển ");
-       System.out.println("3) Thêm, sửa, xóa dữ liệu trong từ điển ");
-       System.out.println("4) Xuất dữ liệu trong từ điển ra file ");
-       System.out.println("5) Thoát ");
+       System.out.println("TU DIEN ANH - VIET");
+       System.out.println("Cac chuc nang : ");
+       System.out.println("1) Tra tu ");
+       System.out.println("2) Hien thi toan bo du lieu trong tu dien ");
+       System.out.println("3) Them, sua, xoa du lieu trong tu dien ");
+       System.out.println("4) Xuat du lieu trong tu dien ra file ");
+       System.out.println("5) Thoat ");
         Scanner sc = new Scanner(System.in);
         int id;
       do { 
-          System.out.print("Nhập lựa chọn chức năng từ điển : ");
+          System.out.print("Nhap lua chon chuc nang trong tu dien : ");
            id = sc.nextInt();
            while (!(id >= 1 && id <=5 ))
        {
@@ -38,11 +38,11 @@ public class Tests {
            }
            case 3 :
            {
-               System.out.println("1) Thêm dữ liệu ");
-               System.out.println("2) Sửa dữ liệu ");
-               System.out.println("3) Xóa dữ liệu ");
-                System.out.println("4) Thoát ");
-               System.out.print("Nhập lựa chọn : ");
+               System.out.println("1) Them du lieu ");
+               System.out.println("2) Sua du lieu ");
+               System.out.println("3) Xoa du lieu ");
+                System.out.println("4) Thoat ");
+               System.out.print("Nhap lua chon : ");
                int a = sc.nextInt();
                while (!(a >= 1 && a <=4 ))
                     {
